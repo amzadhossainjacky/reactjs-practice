0047b1da2f549ae3a682a5f6bddf67c5ccde62f8
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-09-21 15:39:36 +0600

    react pages created and under standing the core boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <>
+      <div>
+        <About />
+        <Contact />
+      </div>
+    </>
+  )
+}
+
+export default App
